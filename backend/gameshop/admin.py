879b124917ab4316725436d49a332ed494410341
h20moumo <h20moumo@du.se>
2023-02-04 19:17:14 +0100

    The Tables and relationships are done

--- backend/gameshop/admin.py
+++ backend/gameshop/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 
-from .models import Games,Administrators
+from .models import Administrators
 
 # Register your models here.
 # admin.site.register(Games)
--- backend/gameshop/models.py
+++ backend/gameshop/models.py
@@ -4,9 +4,12 @@ import datetime
 from django.db import models
 
 # Create your models here.
-class Games(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField(blank=True ,)
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    e_mail = models.CharField(max_length=300)
     created_at = models.DateField(default=datetime.date.today)
     updated_at = models.DateField(default=datetime.date.today)
 
@@ -20,15 +23,15 @@ class Administrators(models.Model):
 class Categories(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    description = models.TextField(blank=True ,)
-    creator_admin_id = models.ForeignKey('Administrators', on_delete=models.CASCADE)
-    deleted_by_admin_id = models.ForeignKey('Administrators', on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+    creator_admin_id = models.ForeignKey(Administrators, on_delete=models.CASCADE, related_name ="creator_admin_id") # *sDjango is unable to generate unique names for many foreign keys so here you should use related_name
+    deleted_by_admin_id = models.ForeignKey(Administrators, on_delete=models.CASCADE, null= True , related_name="deleted_by_admin_id")
     created_at = models.DateField(default=datetime.date.today)
     updated_at = models.DateField(default=datetime.date.today)
 
-class Prod_category(models.Model):
-    prod_id = models.ForeignKey('Product', on_delete=models.CASCADE)
-    car_id = models.ForeignKey('Categories', on_delete=models.CASCADE)
+class Shopping_cart(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
     created_at = models.DateField(default=datetime.date.today)
     updated_at = models.DateField(default=datetime.date.today)
 
@@ -44,6 +47,10 @@ class Product(models.Model):
     deleted_by_admin_id = models.IntegerField(null=True)
     created_at = models.DateField(default=datetime.date.today)
     updated_at = models.DateField(default=datetime.date.today)
+    prod_category = models.ManyToManyField(Categories) # * Many-to-many relationships will create a third table no need to manually creating the third table in this case it created gameshop_prod_cat table.
+    prod_cart = models.ManyToManyField(Shopping_cart)
+
+    
 
 class Discount(models.Model):
     id = models.AutoField(primary_key=True)
@@ -53,7 +60,30 @@ class Discount(models.Model):
     discount_type= models.IntegerField(null=True)
     rate = models.IntegerField(null=True)
     code = models.IntegerField(null=True)
-    admin_id= models.ForeignKey('Administrators', on_delete=models.CASCADE)
+    admin_id= models.ForeignKey(Administrators, on_delete=models.CASCADE)
+    created_at = models.DateField(default=datetime.date.today)
+    updated_at = models.DateField(default=datetime.date.today)
+    
+
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    payment_way = models.CharField(max_length=500)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    discount_code = models.CharField(max_length=8)
+    state = models.BooleanField(default=False)
+    discount_id = models.ForeignKey(Discount, on_delete=models.CASCADE)
+    created_at = models.DateField(default=datetime.date.today)
+    updated_at = models.DateField(default=datetime.date.today)
+
+class Adress(models.Model):
+    id = models.AutoField(primary_key=True)
+    order_id = models.OneToOneField(Order,on_delete=models.CASCADE,null=True)
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    post_number = models.IntegerField(null=True)
+    street_name = models.CharField(max_length=100)
+    co_adress = models.CharField(max_length=100)
     created_at = models.DateField(default=datetime.date.today)
     updated_at = models.DateField(default=datetime.date.today)
 
@@ -65,37 +95,6 @@ class Review(models.Model):
     user_id = models.ForeignKey('User', on_delete=models.CASCADE)
     hidden = models.BooleanField(default=False)
     reported = models.BooleanField(default=False)
-    created_at = models.DateField(default=datetime.date.today)
-    updated_at = models.DateField(default=datetime.date.today)
-
-
-class User(models.Model):
-    id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    e_mail = models.CharField(max_length=300)
-    created_at = models.DateField(default=datetime.date.today)
-    updated_at = models.DateField(default=datetime.date.today)
-    
-class Shopping_cart(models.Model):
-    id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
-    created_at = models.DateField(default=datetime.date.today)
-    updated_at = models.DateField(default=datetime.date.today)
-    
-class Prod_cart(models.Model):
-    cart_id = models.ForeignKey('Shopping_cart', on_delete=models.CASCADE)
-    prod_id = models.ForeignKey('Product', on_delete=models.CASCADE)
-    created_at = models.DateField(default=datetime.date.today)
-    updated_at = models.DateField(default=datetime.date.today)
-    
-class Order(models.Model):
-    id = models.AutoField(primary_key=True)
-    payment_way = models.CharField(max_length=500)
-    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
-    discount_code = models.CharField(max_length=8)
-    state = models.BooleanField(default=False)
-    discount_id = models.ForeignKey('Discount', on_delete=models.CASCADE)
     created_at = models.DateField(default=datetime.date.today)
     updated_at = models.DateField(default=datetime.date.today)
     
