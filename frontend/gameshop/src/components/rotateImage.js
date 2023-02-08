41f424c0c8b26111fdb202bc0133a8825263952b
Kuona <anouk.gouhier-dupuis@etudiant.univ-brest.fr>
2023-02-08 23:56:49 +0100

    Try on the slides pictures

--- frontend/gameshop/src/components/rotateImage.js
+++ frontend/gameshop/src/components/rotateImage.js
@@ -1,37 +1,29 @@
 import React from "react";
-import { Slide } from "react-slideshow-image";
-
-import img1 from "../images/apple.png";
+import "./rotateImage.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import img1 from "../images/img1.jpg";
 import img2 from "../images/img2.jpg";
 import img3 from "../images/img3.jpg";
 
-const prop = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  backgroundSize: "cover",
-  height: "400px",
-};
 
 const RotateImage = () => {
   return (
-    <div Class>
-      <Slide>
-        <div className="eachSlide">
-          {" "}
-          <div>
-            {" "}
-            <div img src={img1} alt="img1" />{" "}
-          </div>{" "}
-        </div>{" "}
-        <div className="eachSlide">
-          <img src={img2} alt="img2" />{" "}
-        </div>{" "}
-        <div className="eachSlide">
-          <img src={img3} alt="img2" />{" "}
-        </div>{" "}
-      </Slide>{" "}
-      <p> add your image </p>{" "}
+    <div className="Slides">
+      <div>
+      <h1>Game of the moment</h1>
+      <Carousel infiniteLoop autoPlay>
+        <div className="image">
+          <img src={img1} alt=""/>
+        </div>
+        <div className="image">
+          <img src={img2} alt=""/>
+        </div>
+        <div className="image">
+          <img src={img3} alt=""/>
+        </div>
+      </Carousel>
+      </div>
     </div>
   );
 };
