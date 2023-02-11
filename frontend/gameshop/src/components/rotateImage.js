25163fbf9243dd69bfffd92f629750bd4dc78951
h20moumo <h20moumo@du.se>
2023-02-11 23:00:40 +0100

    nev

--- frontend/gameshop/src/components/rotateImage.js
+++ frontend/gameshop/src/components/rotateImage.js
@@ -1,58 +1,43 @@
 import React from "react";
-import "./rotateImage.css";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
-import img1 from "../images/img1.jpg";
+import { Slide } from "react-slideshow-image";
+
+import img1 from "../images/apple.png";
 import img2 from "../images/img2.jpg";
 import img3 from "../images/img3.jpg";
 
+const prop = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundSize: "cover",
+  height: "400px",
+};
 
 const RotateImage = () => {
   return (
-<<<<<<< HEAD
     <div Class>
       {" "}
       {/* //{" "}
-                  <Slide>
-                    //{" "}
-                    <div className="eachSlide">
-                      // //{" "}
-                      <div>
-                        // // <div img src={require("../images/apple.png")} alt="img1" /> //{" "}
-                      </div>{" "}
-                      //{" "}
-                    </div>{" "}
-                    //{" "}
-                    <div className="eachSlide">
-                      // <img src={img2} alt="img2" /> //{" "}
-                    </div>{" "}
-                    //{" "}
-                    <div className="eachSlide">
-                      // <img src={img3} alt="img2" /> //{" "}
-                    </div>{" "}
-                    //{" "}
-                  </Slide>{" "}
-                  // <p> add your image </p>{" "} */}{" "}
-=======
-    <div className="Slides">
-      <div>
-      <h1>Games of the moment</h1>
-      <Carousel infiniteLoop autoPlay>
-        <div>
-          <img src={img1} alt=""/>
-          <p className="legend">RPG</p>
-        </div>
-        <div>
-          <img src={img2} alt=""/>
-          <p className="legend">Adventure</p>
-        </div>
-        <div>
-          <img src={img3} alt=""/>
-          <p className="legend">War</p>
-        </div>
-      </Carousel>
-      </div>
->>>>>>> 29bf3506fcd56f6f01b52e23e428c326d3219dcc
+                              <Slide>
+                                //{" "}
+                                <div className="eachSlide">
+                                  // //{" "}
+                                  <div>
+                                    // // <div img src={require("../images/apple.png")} alt="img1" /> //{" "}
+                                  </div>{" "}
+                                  //{" "}
+                                </div>{" "}
+                                //{" "}
+                                <div className="eachSlide">
+                                  // <img src={img2} alt="img2" /> //{" "}
+                                </div>{" "}
+                                //{" "}
+                                <div className="eachSlide">
+                                  // <img src={img3} alt="img2" /> //{" "}
+                                </div>{" "}
+                                //{" "}
+                              </Slide>{" "}
+                              // <p> add your image </p>{" "} */}{" "}
     </div>
   );
 };
