1d4a42eab701d837e1030772fb4b396b86d81e07
h20moumo <h20moumo@du.se>
2023-02-17 13:24:50 +0100

    Mouayad last week tasks done

--- backend/gameshop/urls.py
+++ backend/gameshop/urls.py
@@ -17,6 +17,14 @@ urlpatterns = [
     # * link:  http://127.0.0.1:8000/products/post 
     # * Form : { "name": "prod_1","description": "aösmdöasmdas dlkas dlkas dkas dl askld asld kas","stock":123 }
     path(f'{prodRout}/post', views.postProdcut),
+    # * Method : Delete
+    # * link:  http://127.0.0.1:8000/products/id/delete 
+    path(f'{prodRout}/<int:id>/delete', views.deleteProdcutById),
+    # * Method : PUT
+    # * link:  http://127.0.0.1:8000/products/id/update 
+    # * the data you pass will be uppdated ex : { "name":"mouayad", "description": "uppdated", "stock": 22}
+    path(f'{prodRout}/<int:id>/update', views.updateProductById),
+    
     # * Method : Get
     # * link:  http://127.0.0.1:8000/category
     path(f'{categoryRout}', views.getCategory),
@@ -24,4 +32,12 @@ urlpatterns = [
     # * link:  http://127.0.0.1:8000/category/post 
     # * Form : { "name":"CAT_1","creator_admin_id_id": 1, "description":"asdaaaaaaaaaaaa" }
     path(f'{categoryRout}/post', views.postCategory),
-]  
+    # * Method : Delete
+    # * link:  http://127.0.0.1:8000/category/id/delete 
+    path(f'{categoryRout}/<int:id>/delete', views.deleteCategoryById),
+    # * Method : PUT
+    # * link:  http://127.0.0.1:8000/category/id/update 
+    # * the data you pass will be uppdated ex : { "name":"mouayad", "description": "uppdated"} 
+    path(f'{categoryRout}/<int:id>/update', views.updateCategoryById),
+
+]      
--- backend/gameshop/views.py
+++ backend/gameshop/views.py
@@ -34,7 +34,33 @@ def postProdcut(request):
     except Exception as e:
         return Response(str(e), status= status.HTTP_400_BAD_REQUEST)
 
+@api_view(['DELETE'])
+def deleteProdcutById(request,id):
+    try:
+        num_deleted, _ = Product.objects.filter(id=id).delete()
+        if num_deleted == 0:
+            return Response(status=status.HTTP_404_NOT_FOUND)       
+        else:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+    except Exception as e:
+        return Response(str(e), status= status.HTTP_400_BAD_REQUEST)
 
+@api_view(['PUT'])
+def updateProductById(request,id):
+    try:
+        product = Product.objects.get(id=id)
+        serializer = ProductSerializer(product, data=request.data,partial=True )#? partial=True update only data provided by the body
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+
+# ----------------------------------------------------------------------------------------------------------
 
 @api_view(['GET'])
 def getCategory(request):
@@ -58,3 +84,30 @@ def postCategory(request):
             return Response(serializer.errors)
     except Exception as e:
         return Response(str(e), status= status.HTTP_400_BAD_REQUEST);
+@api_view(['DELETE'])
+def deleteCategoryById(request,id):
+    try:
+        num_deleted, _ = Categories.objects.filter(id=id).delete()
+        if num_deleted == 0:
+            return Response(status=status.HTTP_404_NOT_FOUND)       
+        else:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+    except Exception as e:
+        return Response(str(e), status= status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def updateCategoryById(request,id):
+    try:
+        cat = Categories.objects.get(id=id)
+        serializer = ProductSerializer(cat, data=request.data,partial=True )#? partial=True update only data provided by the body
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+
+# ---------------------------------------------------------------------------------------------- -------------------------------------------
