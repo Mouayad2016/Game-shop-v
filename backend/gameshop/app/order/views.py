3f3fab99ff6e2284e8ca1d44f6c8abede2374c25
h20moumo <h20moumo@du.se>
2023-02-19 14:22:44 +0100

    2

---
+++ backend/gameshop/app/order/views.py
@@ -0,0 +1,53 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from ...models import Order
+from .serializer import OrderSerializer
+
+@api_view(['GET'])
+def getOrders(request):
+    try:
+        orders = Order.objects.all()
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data)
+    except Exception as e:
+        return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def postOrder(request):
+    try:
+        serializer = OrderSerializer(data=request.data)
+        valid = serializer.is_valid()
+        if valid:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def deleteOrderById(request, id):
+    try:
+        num_deleted, _ = Order.objects.filter(id=id).delete()
+        if num_deleted == 0:
+            return Response(status=status.HTTP_404_NOT_FOUND)       
+        else:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+    except Exception as e:
+        return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def updateOrderById(request, id):
+    try:
+        order = Order.objects.get(id=id)
+        serializer = OrderSerializer(order, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except Order.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
