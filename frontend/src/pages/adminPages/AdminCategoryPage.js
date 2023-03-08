6146c8944d654b93263d6f3c73d2d777d3b875bd
Kuona <anouk.gouhier-dupuis@etudiant.univ-brest.fr>
2023-03-08 17:24:23 +0100

    PAge for category

---
+++ frontend/src/pages/adminPages/AdminCategoryPage.js
@@ -0,0 +1,15 @@
+import '../../components/Admin/Admin.css';
+import AdminHeader from '../../components/Admin/AdminHeader'
+import AdminCategory from '../../components/Admin/AdminCategory'
+
+
+function AdminCategoryPage() {
+  return (
+    <div className="App">
+      <AdminHeader />
+      <AdminCategory />
+    </div>
+  );
+}
+
+export default AdminCategoryPage;
