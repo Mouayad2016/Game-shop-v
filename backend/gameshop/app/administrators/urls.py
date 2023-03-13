ada92eba2afb5f67608e1c0ef40b8d6b9a063c96
祈音琉雨 <1515298555@qq.com>
2023-03-14 00:06:05 +0800

    password encryped when create administrator

--- backend/gameshop/app/administrators/urls.py
+++ backend/gameshop/app/administrators/urls.py
@@ -8,20 +8,24 @@ urlpatterns = [
     # ? Note No need to add / att the end i already configured the setting so it will be there automatically
 
     # * Method : Get
-    # * link:  http://127.0.0.1:8000/administrators
+    # * link:  http://127.0.0.1:8000/administrators/get
     path('get', views.getAdministrators),
     # * Method : Get
     # * link:  http://127.0.0.1:8000/administrators/id/get
-    path('getById', views.getAdministratorByID),
+    path('<int:id>/get', views.getAdministratorByID),
+    # * Method : Get
+    # * link:  http://127.0.0.1:8000/administrators/get/username/password
+    # * Form : { "username": "1", "password": "1"}
+    path('get/<str:username>/<str:password>', views.getAdministratorByUsernameAndPassword),
     # * Method : Post 
     # * link:  http://127.0.0.1:8000/administrators/post
-    # * Form : { "username": 1, "password": 1}
+    # * Form : { "username": "1", "password": "1"}
     path('post', views.postAdministrators),
     # * Method : Delete
     # * link:  http://127.0.0.1:8000/administrators/id/delete 
     path('<int:id>/delete', views.deleteAdministratorsById),
     # * Method : PUT
     # * link:  http://127.0.0.1:8000/administrators/id/update 
-    # * the data you pass will be uppdated ex : { "username": 2, "password": 2} 
+    # * the data you pass will be uppdated ex : { "username": "2", "password": "2"} 
     path('<int:id>/update', views.updateAdministratorsById),
 ]      
