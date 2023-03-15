2fab6168c9bb54f591ce2cf6de26dc7b9a17c82c
祈音琉雨 <1515298555@qq.com>
2023-03-15 23:06:02 +0800

    when delete shopping_cart, the data in product_prod_cart will be deleted

--- backend/gameshop/apps.py
+++ backend/gameshop/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class GameshopConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gameshop'
+
+    def ready(self):
+        import app.shopping_cart.signals
