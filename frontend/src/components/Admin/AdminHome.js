62e05f9001b2e63c75e8b2e95893232857197aa9
Loana MOTTAIS <loana.mottais@etudiant.univ-brest.fr>
2023-03-18 15:45:49 +0100

    AdminHome small changes

--- frontend/src/components/Admin/AdminHome.js
+++ frontend/src/components/Admin/AdminHome.js
@@ -34,11 +34,13 @@ const AdminHome = () => {
             </div>
             <div class="row">
               <div class="col">
-                <button class="btn"><Link to="/admin/customers"><i class="fa-solid fa-rectangle-list">Customers</i></Link></button>
+                <button class="btn"><Link to="/admin/customers"><i class="fa-solid fa-rectangle-list"> Customers</i></Link></button>
+              </div>
+              <div class="col">
               </div>
               <div class="col">
                 <button class="btn"><Link to="/admin/order"><i class="fa-solid fa-coins"> Orders</i></Link></button>
-  </div>
+              </div>
             </div>
           </div>
         </div>
