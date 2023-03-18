fe218c8f3baaede766ce1cead189ebd00ef36491
Loana MOTTAIS <loana.mottais@etudiant.univ-brest.fr>
2023-03-18 15:33:22 +0100

    admin profile page nearly finished

--- frontend/src/components/Admin/AdminProfile.js
+++ frontend/src/components/Admin/AdminProfile.js
@@ -4,29 +4,56 @@ import './Admin.css';
 
 const AdminProfile = () => {
 
-
-    const [userData, setUserData] = useState([]);
-    const [admin, setAdmin] = useState();
+    const [admin, setAdmin] = useState([]);
+    const id = sessionStorage.getItem("id");
 
     useEffect(() => {
-        fetchAllAdminData();
+        fetchAdminData();
     }, []);
 
-    const fetchAllAdminData = async () => {
+    const fetchAdminData = async () => {
         try {
-            const response = await axios.get("http://localhost:8000/administrators/get");
-            const admin = response.data.map((admin) => ({
-                id: admin.id,
-                username: admin.username,
-                password: admin.password,
-            }));
-            setUserData(admin);
+            const response = await axios.get(`http://127.0.0.1:8000/administrators/${id}/get`);
+            const admin = response.data;
+            setAdmin(admin);
         } catch (error) {
             console.log(error);
         }
     };
 
-    const id = sessionStorage.getItem("id");
+    const [formData, setFormData] = useState({
+        username: admin.username,
+        password: admin.password, 
+      });
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+          const response = await axios.put(
+            `http://127.0.0.1:8000/administrators/${id}/update`,
+            formData
+          );
+          console.log(response.data);
+          setFormData({
+            username: admin.username,
+            password: admin.password,
+          });
+          setErrorMessage("You just changed your profile.");
+          window.location.reload(false);
+        } catch (error) {
+          console.error(error);
+          setErrorMessage("An error occurred while updating your profile.");
+        }
+    };
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+          ...formData,
+          [name]: value,
+        });
+    };
     
     return (
         <div class="adminProducts">
@@ -38,14 +65,10 @@ const AdminProfile = () => {
                         <br></br>
                         </thead>
                         <tbody>
-                        {userData.filter((e) => {
-                            return e.id === parseInt(id) ? e : null;
-                        }).map((e) =>(
-                            <tr>
-                            <td>Username : </td>
-                            <td>{e.username}</td>
-                            </tr>
-                        ))}
+                        <tr>
+                        <td>Username : </td>
+                        <td>{admin.username}</td>
+                        </tr>
                         <tr>
                         <td>Password : </td>
                         <td><i>Your password is encrypted and cannot be shown</i></td>
@@ -54,9 +77,46 @@ const AdminProfile = () => {
                     </table>
                 </div>
             </div>
-            <div class="container">
-                
-            </div>
+
+            <section>
+                <br></br>
+                <br></br>
+                <div class="col-lg-12"><h3>Change your profile</h3></div>
+                <div className="form_container">
+                    <form onSubmit={handleSubmit}>
+                        {errorMessage && (
+                        <div className="alert alert-danger" role="alert">
+                            {errorMessage}
+                        </div>
+                        )}
+                        <div className="form-group">
+                        <label htmlFor="name">Username</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="username"
+                            name="username"
+                            value={formData.username}
+                            onChange={handleInputChange}
+                        />
+                        </div>
+                        <div className="form-group">
+                        <label htmlFor="description">New password</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleInputChange}
+                        />
+                        </div>
+                        <button type="submit" className="btn btn-primary">
+                        Modify 
+                        </button>
+                    </form>
+                </div>
+            </section>
         </div>
     );
 
